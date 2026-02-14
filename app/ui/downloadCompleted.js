4f07a0d14928e8b09335937144506c5c31d62f81
Zach Selland <zach@hfa3.org>
2026-02-14 07:42:36 -0800

    change logo, change some text, remove "try send" button, change footer

--- app/ui/downloadCompleted.js
+++ app/ui/downloadCompleted.js
@@ -1,8 +1,7 @@
 const html = require('choo/html');
 const assets = require('../../common/assets');
 
-module.exports = function(state) {
-  const btnText = state.user.loggedIn ? 'okButton' : 'sendYourFilesLink';
+module.exports = function (state) {
   return html`
     <div
       id="download-complete"
@@ -14,19 +13,6 @@ module.exports = function(state) {
       <svg class="my-8 h-48 text-primary">
         <use xlink:href="${assets.get('completed.svg')}#Page-1" />
       </svg>
-      <p
-        class="text-grey-80 leading-normal dark:text-grey-40 ${state.user
-          .loggedIn
-          ? 'hidden'
-          : ''}"
-      >
-        ${state.translate('trySendDescription')}
-      </p>
-      <p class="my-5">
-        <a href="/" class="btn rounded-lg flex items-center mt-4" role="button"
-          >${state.translate(btnText)}</a
-        >
-      </p>
     </div>
   `;
 };
